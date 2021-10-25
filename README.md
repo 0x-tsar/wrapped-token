af5d7864a7cf210a5eae7961e7c8dc31d3a8ecdd
Gustavo Fernandes <contato.gustavofernandes@hotmail.com>
2021-10-25 14:17:42 -0300

    :balloon:

---
+++ README.md
@@ -0,0 +1,1 @@
+run Ganache on port 7545 and run `truffle exec scripts/script.js`
--- scripts/script.js
+++ scripts/script.js
@@ -10,15 +10,18 @@ module.exports = async () => {
     console.log(`balance ETH before: ${ethBalance}`);
     console.log(`balance weth before: ${wrappedBalance}`);
     await wrappedEther.mintWeth({ from: account, value: value });
-    console.log(`balance after: ${wrappedBalance}`);
-
-    console.log("------");
-    console.log("now getting Eth back");
-    await wrappedEther.retrieveEther(value);
     wrappedBalance = await wrappedEther.balanceOf(account);
     ethBalance = await web3.eth.getBalance(account);
     console.log(`balance ETH after: ${ethBalance}`);
     console.log(`balance weth after: ${wrappedBalance}`);
+    console.log("------");
+    console.log("now getting Eth back");
+    // //
+    await wrappedEther.retrieveEther(value);
+    wrappedBalance = await wrappedEther.balanceOf(account);
+    ethBalance = await web3.eth.getBalance(account);
+    console.log(`balance ETH final: ${ethBalance}`);
+    console.log(`balance weth final: ${wrappedBalance}`);
   } catch (error) {
     console.log(error);
   }
